2f4a0c5793fd440eb5cad122e414eb4f09ade090
vaibhavkoshta21 <101541709+vaibhavkoshta21@users.noreply.github.com>
2022-11-03 04:17:36 -0700

    Create User.java

---
+++ src/main/java/com/greatlearning/EmployeeManagement/entity/User.java
@@ -0,0 +1,49 @@
+package com.greatlearning.EmployeeManagement.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="user_id")
+	private Long id;
+	
+	@Column(name = "username")
+	private String username ;
+	
+	@Column(name = "password")
+	private String password ;
+	
+	@ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+	@JoinTable(name= "users_roles",joinColumns =@JoinColumn(name="user_id")
+	,inverseJoinColumns = @JoinColumn(name = "role_id"))	
+	private List<Role> roles = new ArrayList<>();
+
+}
