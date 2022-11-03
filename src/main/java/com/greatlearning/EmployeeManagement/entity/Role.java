c494553ae3499a9e26b7722c2b431cafd451c7ff
vaibhavkoshta21 <101541709+vaibhavkoshta21@users.noreply.github.com>
2022-11-03 04:17:04 -0700

    Create Role.java

---
+++ src/main/java/com/greatlearning/EmployeeManagement/entity/Role.java
@@ -0,0 +1,30 @@
+package com.greatlearning.EmployeeManagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Data
+@Getter
+@Setter
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="role_id")
+	private Integer id;
+	
+	@Column(name="name")
+	private String name;
+
+
+}
