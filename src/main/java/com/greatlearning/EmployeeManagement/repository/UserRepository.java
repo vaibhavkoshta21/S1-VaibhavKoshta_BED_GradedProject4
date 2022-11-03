8b67674fadc48349ba988e140975a228bab8984c
vaibhavkoshta21 <101541709+vaibhavkoshta21@users.noreply.github.com>
2022-11-03 04:19:30 -0700

    Create UserRepository.java

---
+++ src/main/java/com/greatlearning/EmployeeManagement/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.greatlearning.EmployeeManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.greatlearning.EmployeeManagement.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	@Query("SELECT u FROM User u WHERE u.username = ?1")
+	public User getUserByUsername(String username);
+
+}
